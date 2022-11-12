5d8bf7925404751119e2882363e4027a6f9572fc
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-11-12 10:16:11 -0300

    WIP integers

--- Vero/Syntax/Core/Data.lean
+++ Vero/Syntax/Core/Data.lean
@@ -13,20 +13,59 @@ def MUL  := ⟦λ m n f. m (n f)⟧
 def SUCC := ⟦λ n f x. f (n f x)⟧
 def PRED := ⟦λ n f x. n (λ g h. h (g f)) (λ u. x) (λ u. u)⟧
 def SUB  := ⟦λ m n. n $PRED m⟧
+def DIV  := ⟦TODO⟧
+-- def DIV  := ⟦λ n. ((λ f. (λ x. x x) (λ x. f (x x)))
+--                     (λ c. λ n. λ m. λ f. λ x.
+--                       (λ d. (λ n. n (λ x. (λ a. λ b. b)) (λ a.λ b. a)) d ((λ f.λ x. x) f x) (f (c d m f x)))
+--                       ((λ m. λ n. n (λ n. λ f. λ x. n (λ g.λ h. h (g f)) (λ u. x) (λ u. u)) m) n m)))
+--                   ((λ n. λ f. λ x. f (n f x)) n)⟧
 
 end NAT
 
 namespace BOOL
 
-def TRUE   := ⟦λ x y. x⟧
-def FALSE  := ⟦λ x y. y⟧
-def AND    := ⟦λ p q. p q p⟧
-def OR     := ⟦λ p q. p p q⟧
-def NOT    := ⟦λ p. p $FALSE $TRUE⟧
-def FORK   := ⟦λ p a b. p a b⟧
-def ISZERO := ⟦λ n. n (λ x. $FALSE) $TRUE⟧
-def LE     := ⟦λ m n. $ISZERO ($NAT.SUB m n)⟧
+def TRUE  := ⟦λ x y. x⟧
+def FALSE := ⟦λ x y. y⟧
+def AND   := ⟦λ p q. p q p⟧
+def OR    := ⟦λ p q. p p q⟧
+def NOT   := ⟦λ p a b. p b a⟧
+def XOR   := ⟦λ a b. a ($NOT b) b⟧
+
+def ISZ := ⟦λ n. n (λ x. $FALSE) $TRUE⟧
+def LE  := ⟦λ m n. $ISZ ($NAT.SUB m n)⟧
+def EQ  := ⟦λ m n. $AND ($LE m n) ($LE n m)⟧
 
 end BOOL
+
+def FLOW.FORK := ⟦λ p a b. p a b⟧
+
+namespace PAIR
+
+def PROD := ⟦λ a b f. f a b⟧
+def FST  := ⟦λ p. p (λ x y. x)⟧
+def SND  := ⟦λ p. p (λ x y. y)⟧
+
+end PAIR
+
+/--
+A `true` on the first component means a negative number. This simplifies
+multiplication and division by avoiding a `not` on the `xor` for the signal.
+-/
+def INT : Int → AST
+  | .ofNat   n => ⟦$PAIR.PROD $BOOL.FALSE $(NAT n)⟧
+  | .negSucc n => ⟦$PAIR.PROD $BOOL.TRUE  $(NAT (n + 1))⟧
+
+namespace INT
+
+open PAIR BOOL
+
+def NEG := ⟦λ a. $PROD ($NOT ($FST a)) ($SND a)⟧
+def MUL := ⟦λ a b. $PROD ($XOR ($FST a) ($FST b)) ($NAT.MUL ($SND a) ($SND b))⟧
+def DIV := ⟦λ a b. $PROD ($XOR ($FST a) ($FST b)) ($NAT.DIV ($SND a) ($SND b))⟧
+
+def ADD := ⟦TODO⟧
+def SUB := ⟦λ a b. $ADD a ($NEG b)⟧
+
+end INT
 
 end Vero.Syntax.Core.AST
--- Vero/Syntax/Core/ReduceTo.lean
+++ Vero/Syntax/Core/ReduceTo.lean
@@ -1,16 +1,30 @@
 import Vero.Syntax.Core.Expr
+import Vero.Syntax.Core.Data
 
 namespace Vero.Syntax.Core
 
 inductive ValType
-  | nat | bool | any
+  | any
+  | nat
+  | bool
+  | pair : ValType → ValType → ValType
+  | int
   deriving Repr
 
 inductive Value
-  | nat : Nat → Value
-  | bool : Bool → Value
   | expr : Expr → Value
-  deriving Repr
+  | nat  : Nat → Value
+  | bool : Bool → Value
+  | pair : Value → Value → Value
+  | int  : Int → Value
+  deriving Repr, Inhabited
+
+protected def Value.toString : Value → String
+  | .expr e => toString e
+  | .nat  n => toString n
+  | .bool b => toString b
+  | .pair f s => s!"({f.toString} . {s.toString})"
+  | .int  i => toString i
 
 namespace Expr
 
@@ -28,6 +42,40 @@ def toBool : Expr → Except Expr Bool
   | lam (lam (var 1)) => return true
   | x => throw x
 
+mutual
+
+  partial def ofType (e : Expr) : ValType → Value
+    | .any => .expr e
+    | .nat => match e.toNat with
+      | .ok n => .nat n
+      | .error e => .expr e
+    | .bool => match e.toBool with
+      | .ok b => .bool b
+      | .error e => .expr e
+    | .pair t₁ t₂ => match e.toPair t₁ t₂ with
+      | .ok (f, s) => .pair f s
+      | .error e => .expr e
+    | .int => match e.toInt with
+      | .ok i => .int i
+      | .error e => .expr e
+
+  partial def toPair (a b : ValType) : Expr → Except Expr (Value × Value)
+    | lam (app (app (var 0) x) (y)) =>
+      let t₁ : Value := x.ofType a
+      let t₂ : Value := y.ofType b
+      return (t₁, t₂)
+    | x => throw x
+  
+  partial def toInt (e : Expr) : Except Expr Int :=
+    match e.toPair .bool .nat with
+    | .ok (.bool b, .nat n) => match b with
+      | false => return .ofNat n
+      | true  => return .negSucc (n - 1)
+    | .ok _
+    | .error e => throw e
+
+end
+
 end Expr
 
 namespace AST
@@ -38,15 +86,11 @@ Tries to reduce an AST to a certain type. Returns `.expr` in case of failure.
 def reduceTo (x : AST) (type : ValType) : Except String Value :=
   match x.toExpr with
   | .error err => throw err
-  | .ok expr =>
-    let expr := expr.reduce
-    return match type with
-    | .nat => match expr.toNat with
-      | .ok n => .nat n
-      | .error e => .expr e
-    | .bool =>  match expr.toBool with
-      | .ok b => .bool b
-      | .error e => .expr e
-    | .any => .expr expr
+  | .ok expr => return expr.reduce.ofType type
+
+def reduceToPP (x : AST) (type : ValType) : String :=
+  match x.reduceTo type with
+  | .error err => err
+  | .ok v => v.toString
 
 end Vero.Syntax.Core.AST
