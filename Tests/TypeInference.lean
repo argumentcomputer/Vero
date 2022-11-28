fdc5bac37af803521259b4dfa99d95d5f2ceddf9
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-11-28 19:31:01 -0300

    improved syntax

--- Tests/TypeInference.lean
+++ Tests/TypeInference.lean
@@ -3,33 +3,33 @@ import Vero.Frontend.DSL
 
 open Vero Frontend.DSL
 
--- def pairs : List $ Frontend.AST × (Option Typ) := [
---   (⟦3⟧, some .nat),
---   (⟦3 + +3⟧, none),
---   (⟦3 + x⟧, some .nat),
---   (⟦x + +3⟧, some .int),
---   (⟦x + tt⟧, none),
---   (⟦ff & tt⟧, some .bool),
---   (⟦(x => x) @ 1⟧, some ⟪nat⟫),
---   (⟦f : _ -> nat := x => x; f @ 3⟧, some ⟪nat⟫),
---   (⟦f : nat -> int -> nat := n y => n; f⟧,  some ⟪nat -> int -> nat⟫),
---   (⟦(x : nat) y => x + y⟧, some ⟪nat -> nat -> nat⟫),
---   (⟦f : _ -> _ -> _ := x (y : nat) => x + y; f⟧, some ⟪nat -> nat -> nat⟫),
---   (⟦f x (y : nat) := x + y; f⟧, some ⟪nat -> nat -> nat⟫),
---   (⟦f (x : nat) y := x + y; f⟧, some ⟪nat -> nat -> nat⟫),
---   (⟦(x (y : int) => x) @ 1⟧, some ⟪int -> nat⟫),
---   (⟦x (f : nat -> int) => f @ x⟧, some ⟪nat -> (nat -> int) -> int⟫),
---   (⟦x (f : _ -> int) => f @ (x : bool)⟧, some ⟪bool -> (bool -> int) -> int⟫),
---   (⟦f : nat -> _ := x => x; f @ 3⟧, some ⟪nat⟫),
---   (⟦f : nat -> int -> _ := n y => y; f⟧,  some ⟪nat -> int -> int⟫)
--- ]
+def pairs : List $ Frontend.AST × (Option Typ) := [
+  (⟦3⟧, some .nat),
+  (⟦3 + +3⟧, none),
+  (⟦3 + x⟧, some .nat),
+  (⟦x + +3⟧, some .int),
+  (⟦x + tt⟧, none),
+  (⟦ff & tt⟧, some .bool),
+  (⟦(fun x => x) 1⟧, some ⟪nat⟫),
+  (⟦let f : _ -> nat := fun x => x; f 3⟧, some ⟪nat⟫),
+  (⟦let f : nat -> int -> nat := fun n y => n; f⟧,  some ⟪nat -> int -> nat⟫),
+  (⟦fun (x : nat) y => x + y⟧, some ⟪nat -> nat -> nat⟫),
+  (⟦let f : _ -> _ -> _ := fun x (y : nat) => x + y; f⟧, some ⟪nat -> nat -> nat⟫),
+  (⟦let f x (y : nat) := x + y; f⟧, some ⟪nat -> nat -> nat⟫),
+  (⟦let f (x : nat) y := x + y; f⟧, some ⟪nat -> nat -> nat⟫),
+  (⟦(fun x (y : int) => x) 1⟧, some ⟪int -> nat⟫),
+  (⟦fun x (f : nat -> int) => f x⟧, some ⟪nat -> (nat -> int) -> int⟫),
+  (⟦fun x (f : _ -> int) => f (x : bool)⟧, some ⟪bool -> (bool -> int) -> int⟫),
+  (⟦let f : nat -> _ := fun x => x; f 3⟧, some ⟪nat⟫),
+  (⟦let f : nat -> int -> _ := fun n y => y; f⟧,  some ⟪nat -> int -> int⟫)
+]
 
 -- TODO : fix
-def pairs : List $ Frontend.AST × (Option Typ) := [
-  (⟦f := x => x; f @ 3⟧, some ⟪nat⟫)
-  -- (⟦f : _ -> _ := x => x; f @ 3⟧, some ⟪nat⟫)
-  -- (⟦f : _ := x => x; f @ 3⟧, some ⟪nat⟫)
-]
+-- def pairs : List $ Frontend.AST × (Option Typ) := [
+--   (⟦let f := fun x => x; f 3⟧, some ⟪nat⟫),
+--   (⟦let f : _ -> _ := fun x => x; f 3⟧, some ⟪nat⟫),
+--   (⟦let f : _ := fun x => x; f 3⟧, some ⟪nat⟫)
+-- ]
 
 open LSpec in
 def main := lspecIO $
@@ -39,4 +39,3 @@ def main := lspecIO $
         withExceptOk "Type inference succeeds" ast.inferTyp fun gotTyp =>
           test s!"Expected {expecTyp} equals {gotTyp}" (expecTyp == gotTyp)
       | none => withExceptError "Type inference fails" ast.inferTyp fun _ => .done
-#eval main
--- Vero/Frontend/AST.lean
+++ Vero/Frontend/AST.lean
@@ -96,10 +96,10 @@ partial def AST.fillHoles (ctx : Ctx) : AST → Typ → Except String AST
         return .lam ⟨s, sTyp⟩ b
       | _ => throw ""
     | .app f a => do match ← f.inferTyp ctx with
-      | .hole => -- is this needed?
-        let aTyp ← a.inferTyp ctx
-        let f ← f.fillHoles ctx (.pi aTyp typ)
-        return .app f a
+      -- | .hole => -- is this needed?
+      --   let aTyp ← a.inferTyp ctx
+      --   let f ← f.fillHoles ctx (.pi aTyp typ)
+      --   return .app f a
       | .pi iTyp oTyp =>
         let oTyp ← unify oTyp typ
         let iTyp ← unify iTyp (← a.inferTyp ctx)
