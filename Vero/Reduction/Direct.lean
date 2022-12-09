690d53b76460a4ef4c0c0ce1805abc0fa74a019c
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-12-09 19:21:44 -0300

    WIP

--- Vero/Reduction/Direct.lean
+++ Vero/Reduction/Direct.lean
@@ -1,25 +1,48 @@
 import Vero.Common.Typ
 import Vero.Common.Expr
 
-namespace Vero.Expr
+namespace Vero
 
-def shift (dep inc : Nat) : Expr → Expr
-  | .var n => if n >= dep then .var (n + inc) else .var n
-  | .lam b => .lam (shift (dep + 1) inc b)
-  | .app x y => .app (shift dep inc x) (shift dep inc y)
+inductive Norm where
+  | lam : Expr → List Norm → Norm
+  | neu : Nat  → List Norm → Norm
+  deriving Inhabited
 
-def subst (dep : Nat) (arg : Expr) : Expr → Expr
-  | .var n => match compare n dep with
-    | .lt => .var n
-    | .eq => shift 0 dep arg
-    | .gt => .var (n - 1)
-  | .lam b => .lam (subst (dep + 1) arg b)
-  | .app x y => .app (subst dep arg x) (subst dep arg y)
+namespace Expr
 
-partial def reduce : Expr → Expr
-  | .app fnc arg => match reduce fnc with
-    | .lam bod => reduce (subst 0 arg.reduce bod)
-    | fnc' => .app fnc' arg.reduce
-  | x => x
+mutual
+
+  partial def eval (env : List Norm) : Expr → Norm
+    | .var j => env.getD j $ .neu (j - env.length) []
+    | .lam bod => .lam bod env
+    | .app fnc arg => apply (fnc.eval env) (arg.eval env)
+
+  partial def apply (fnc arg : Norm) : Norm :=
+    match fnc with
+    | .lam bod env => bod.eval (arg :: env)
+    | .neu hd args => .neu hd (arg :: args)
+
+end
+
+mutual
+
+  partial def quote (val : Norm) (shift : Nat) : Expr := match val with
+    | .lam bod env => .lam $ bod.inst env 1 shift
+    | .neu hd args =>
+      args.foldr (fun a f => .app f $ quote a shift) (.var (hd + shift))
+
+  partial def inst (env : List Norm) (dep shift : Nat) : Expr → Expr
+    | .app fnc arg => .app (fnc.inst env dep shift) (arg.inst env dep shift)
+    | .lam bod => .lam $ bod.inst env dep.succ shift
+    | t@(.var j) =>
+      if j < dep then t else
+      match env.get? (j - dep) with
+      | some val => quote val (shift + dep)
+      | none     => .var (j - dep - env.length)
+
+end
+
+def reduce (e : Expr) : Expr :=
+  quote (e.eval []) 0
 
 end Vero.Expr
--- Vero/Reduction/Scalar.lean
+++ Vero/Reduction/Scalar.lean
@@ -1,11 +1,14 @@
 import Vero.Scalar.Datatypes
 import Vero.Scalar.Utils
+import YatimaStdLib.Ord
 
 namespace Vero.Scalar
 
 structure EvalState where
   store : StoreF
-  cache : Std.RBMap Ptr Ptr compare
+  evalCache  : Std.RBMap (Ptr × Ptr) Ptr compare
+  applyCache : Std.RBMap (Ptr × Ptr) Ptr compare
+  deriving Inhabited
 
 abbrev ReduceM := ExceptT String $ StateM EvalState
 
@@ -19,65 +22,71 @@ def getLamBody (ptr : Ptr) : ReduceM Ptr := do
   | .lam b => pure b
   | x => throw s!"expected a lam expression but got {x}"
 
-def getAppFunArg (ptr : Ptr) : ReduceM (Ptr × Ptr) := do
+def getAppFncArg (ptr : Ptr) : ReduceM (Ptr × Ptr) := do
   match ← getExpr ptr with
   | .app f a => pure (f, a)
   | x => throw s!"expected an app expression but got {x}"
 
-def ReduceM.addExprHash (ptr : Ptr) (expr : ExprF) : ReduceM Ptr :=
-  modifyGet fun stt => (ptr, { stt with store := stt.store.insert ptr expr })
+def getNormLamBodyEnv (ptr : Ptr) : ReduceM (Ptr × Ptr) := do
+  match ← getExpr ptr with
+  | .normLam b e => pure (b, e)
+  | x => throw s!"expected a normLam expression but got {x}"
 
-open ReduceM
+def getNormNeuFncArgs (ptr : Ptr) : ReduceM (F × Ptr) := do
+  match ← getExpr ptr with
+  | .normNeu v a => pure (v, a)
+  | x => throw s!"expected a normLam expression but got {x}"
 
-partial def shiftM (dep inc : F) (ptr : Ptr) : ReduceM Ptr :=
-  match ptr.tag with
-  | .var =>
-    if ptr.val >= dep then
-      let n := ptr.val + inc
-      addExprHash ⟨.var, n⟩ (.var n)
-    else return ptr
-  | .lam => do
-    let b ← getLamBody ptr
-    let b ← shiftM dep.succ inc b
-    addExprHash ⟨.lam, hashPtr b⟩ (.lam b)
-  | .app => do
-    let (f, a) ← getAppFunArg ptr
-    let f ← shiftM dep inc f
-    let a ← shiftM dep inc a
-    addExprHash ⟨.app, hashPtrPair f a⟩ (.app f a)
+def ReduceM.addExprF (ptr : Ptr) (exprF : ExprF) : ReduceM Ptr :=
+  modifyGet fun stt => (ptr, { stt with store := stt.store.insert ptr exprF })
 
-partial def substM (dep : F) (arg target : Ptr) : ReduceM Ptr :=
-  match target.tag with
-  | .var => match compare target.val dep with
-    | .lt => pure target
-    | .eq => shiftM F.zero dep arg
-    | .gt => let n := target.val.pred; addExprHash ⟨.var, n⟩ (.var n)
-  | .lam => do
-    let b ← getLamBody target
-    let b ← substM dep.succ arg b
-    addExprHash ⟨.lam, hashPtr b⟩ (.lam b)
-  | .app => do
-    let (f, a) ← getAppFunArg target
-    let f ← substM dep arg f
-    let a ← substM dep arg a
-    addExprHash ⟨.app, hashPtrPair f a⟩ (.app f a)
+mutual
 
-partial def reduceM (ptr : Ptr) : ReduceM Ptr :=
-  match ptr.tag with
-  | .var | .lam => return ptr
-  | .app => do match (← get).cache.find? ptr with
-    | some ptr => return ptr
-    | none =>
-      let (f, a) ← getAppFunArg ptr
-      let f ← reduceM f
-      let ptr' ← match f.tag with
-        | .lam => reduceM $ ← substM F.zero (← reduceM a) (← getLamBody f)
-        | _ => let a ← reduceM a; addExprHash ⟨.app, hashPtrPair f a⟩ (.app f a)
-      modifyGet fun stt => (ptr', { stt with cache := stt.cache.insert ptr ptr' })
+open ReduceM (addExprF)
+
+partial def evalM (exprPtr envPtr : Ptr) : ReduceM Ptr := do
+  match (← get).evalCache.find? (exprPtr, envPtr) with
+  | some normPtr => pure normPtr
+  | none =>
+    let normPtr ← match exprPtr.tag with
+      | .var => sorry
+      | .lam =>
+        let bodPtr ← getLamBody exprPtr
+        addExprF ⟨.normLam, hashPtrPair bodPtr envPtr⟩ (.normLam bodPtr envPtr)
+      | .app =>
+        let (fncPtr, argPtr) ← getAppFncArg exprPtr
+        applyM (← evalM fncPtr envPtr) (← evalM argPtr envPtr)
+      | _ => throw s!"invalid pointer for evaluation: {exprPtr}"
+    modifyGet fun stt => (normPtr, { stt with
+      evalCache := stt.evalCache.insert (exprPtr, envPtr) normPtr })
+
+partial def applyM (fncPtr argPtr : Ptr) : ReduceM Ptr := do
+  match (← get).applyCache.find? (fncPtr, argPtr) with
+  | some normPtr => pure normPtr
+  | none =>
+    let normPtr ← match fncPtr.tag with
+      | .normLam => sorry
+      | .normNeu => sorry
+      | _ => throw s!"invalid pointer for application function: {fncPtr}"
+    modifyGet fun stt => (normPtr, { stt with
+      evalCache := stt.evalCache.insert (fncPtr, argPtr) normPtr })
+
+end
+
+mutual
+
+partial def quoteM (normPtr : Ptr) (shift : Nat) : ReduceM Ptr := sorry
+
+partial def instM (exprPtr envPtr : Ptr) (dep shift : Nat) : ReduceM Ptr := sorry
+
+end
+
+def reduceM (ptr : Ptr) : ReduceM Ptr := do
+  quoteM (← evalM ptr ⟨.envNil, .zero⟩) 0
 
 def reduce (ptr : Ptr) (store : StoreF) : Except String (Ptr × StoreF) :=
-  match StateT.run (reduceM ptr) ⟨store, default⟩ with
+  match StateT.run (reduceM ptr) ⟨store, default, default⟩ with
   | (.error err, _) => .error err
-  | (.ok ptr, ⟨store, _⟩) => .ok (ptr, store)
+  | (.ok ptr, ⟨store, _, _⟩) => .ok (ptr, store)
 
 end Vero.Scalar
